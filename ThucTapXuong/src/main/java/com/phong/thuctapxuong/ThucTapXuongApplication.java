98e8ff333bd11301b903b8a8d4ac78745247a65c
kkkkkkkkikikkkki <kietnguyenhoang155@gmail.com>
2024-07-08 22:33:52 +0700

    Merge ThucTapXuong from main branch

---
+++ ThucTapXuong/src/main/java/com/phong/thuctapxuong/ThucTapXuongApplication.java
@@ -0,0 +1,13 @@
+package com.phong.thuctapxuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThucTapXuongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThucTapXuongApplication.class, args);
+    }
+
+}
